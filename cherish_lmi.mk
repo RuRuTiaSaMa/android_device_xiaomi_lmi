50ab7616dec3f5c5e50c474ac8d6a0911832f599
RuRuTiaSaMa <1009087450@qq.com>
2022-11-06 11:25:44 +0800

    cherish bringup
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- cherish_lmi.mk
+++ cherish_lmi.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelPlus-UI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := aosp_lmi
+PRODUCT_NAME := cherish_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -30,7 +30,7 @@ BUILD_FINGERPRINT := Redmi/lmi/lmi:12/RKQ1.211001.001/V13.0.3.0.SJKMIXM:user/rel
 # VoidUI Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
-EXTRA_FOD_ANIMATIONS := true
+EXTRA_UDFPS_ANIMATIONS := true
 IS_PHONE := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -38,4 +38,7 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_CALL_RECORDING := true
-VOID_MAINTAINER := RuRuTiaSaMa
+
+# Maintainer Name
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=RuRuTiaSaMa
